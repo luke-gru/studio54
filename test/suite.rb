5e01b74c182ee55b8f269e90eaab063922d873b3
Luke Gruber <luke.gru@gmail.com>
2011-10-10 19:41:09 -0400

    add proper .gitignore cascade to include rack dirs

--- test/suite.rb
+++ test/suite.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 # environment file
-require File.join(File.expand_path(File.dirname(__FILE__) + '/..'), "config/environment")
+require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'config/environment')
 
 # minitest is default
 require 'minitest/autorun'
-########################
+###
 # require 'minitest/spec'
 # require 'test/unit'
 # require 'shoulda'
