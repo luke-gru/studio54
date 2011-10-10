9d3b7344a167d8b3261d4c23e4cb057aa3d2a27d
Luke Gru <luke@luke-Pavilion-dv4000-PX311UA-ABL.(none)>
2011-10-09 23:00:07 -0400

    change app to be 'sinatra modular'

--- dance.rb
+++ dance.rb
@@ -1,15 +1,45 @@
 require "sinatra"
-$:.unshift File.expand_path('.') unless $:.include? File.expand_path('.')
+cur_dir = File.expand_path File.dirname(__FILE__)
+$:.unshift cur_dir unless $:.include? cur_dir
 require 'config/environment'
-include Studio54::Config::Environment
 # lots of requires found in app_tie
 require 'app_tie'
-include LazyController::Routable
 
-set :views, settings.root + '/public'
+# these middlewares need to go to their own dir/
+class Studio54::SinatraTastes < Sinatra::Base
+  set :environment, "development"
 
-get '/' do
-  context = controller :users, :index
-  erb :index, {}, context
+  configure do
+    set :views, settings.root + '/public'
+    set :logging, true
+    set :static, true
+    set :method_override, true
+    set :inline_template, true
+  end
+
+  configure :production do end
+  configure :development do end
+  configure :test do end
 end
+
+class Studio54::Router < Sinatra::Base
+  include ::Studio54::Config::Environment
+  include ::LazyController::Routable
+
+  get '/' do
+    context = controller :users, :index
+    erb :index, {}, context
+  end
+end
+
+# base class for app
+class Studio54::Dancefloor < Sinatra::Base
+  include Studio54::Config::Environment
+  use ::Studio54::SinatraConfig
+  use ::Studio54::Router
+
+  run! if app_file == $0
+
+end
+
 
